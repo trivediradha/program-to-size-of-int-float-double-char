fe78847c032771a4bb49dc4a6e8bb06b8767abe8
trivediradha <86192184+trivediradha@users.noreply.github.com>
2021-08-30 21:44:27 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Size of int is:"<<sizeof(int)<<"bytes"<<endl;
+    cout<<"Size of float is:"<<sizeof(float)<<"bytes"<<endl;
+    cout<<"Size of double is:"<<sizeof(double)<<"bytes"<<endl;
+    cout<<"Size of char is"<<sizeof(char)<<"bytes"<<endl;
+    return 0;
+}
+
+
